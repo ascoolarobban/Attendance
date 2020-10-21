ec7eea22ca4cf9bd4047f0a7adcc41f241b2f595
Robin Ellingsen <70570646+ascoolarobban@users.noreply.github.com>
2020-10-21 12:32:30 +0200

    Initial Commit

---
+++ Attendance/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Attendance
+//
+//  Created by Robin Ellingsen on 2020-10-21.
+//  Copyright © 2020 Robin Ellingsen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
